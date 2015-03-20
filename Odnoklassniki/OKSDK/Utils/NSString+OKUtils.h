63a204caec3f8148cc93c1b39b330e16809d022f
baydet <alexander.evsyuchenya@orangesoft.by>
2015-03-20 16:13:16 +0300

    initial commit[2]

---
+++ Odnoklassniki/OKSDK/Utils/NSString+OKUtils.h
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+
+@interface NSString (OKUtils)
+
+- (NSString *)md5;
+
+- (NSDictionary *)dictionaryByParsingURLQueryPart;
+
+- (NSString *)stringByURLDecodingString;
+
+- (NSString *)URLEncodedString;
+
+@end
